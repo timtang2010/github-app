e555426dfcc2c5e1486ed21d7f7d96e11b1e6d68
timtang2010 <timtang@126.com>
2014-04-23 10:48:58 +0800

    新改进
    新改进

---
+++ AD/AD/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AD
+//
+//  Created by yuxin tang on 14-4-22.
+//  Copyright (c) 2014年 v5mcs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AD/AD/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AD
+//
+//  Created by yuxin tang on 14-4-22.
+//  Copyright (c) 2014年 v5mcs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
